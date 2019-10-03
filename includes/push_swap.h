ae30a798ce4c5f7a322e7c1beabb540f16ca639b
ekedge-w <alicemirror1211@gmail.com>
2019-10-03 20:50:45 +0300

    Разраб. сортировки для 0-5 эл-в

---
+++ includes/push_swap.h
@@ -0,0 +1,49 @@
+#ifndef PUSH_SWAP_PUSH_SWAP_H
+#define PUSH_SWAP_PUSH_SWAP_H
+
+typedef struct	s_double_link_lst
+{
+    int							data;
+    struct s_double_link_lst	*next;
+    struct s_double_link_lst	*prev;
+}				t_dlst;
+
+void sa(t_dlst **head_a);
+void sb(t_dlst **head_b);
+void ss(t_dlst **head_a, t_dlst **head_b);
+
+void ra(t_dlst **head_a);
+void rb(t_dlst **head_b);
+void rr(t_dlst **head_a, t_dlst **head_b);
+
+void rra(t_dlst **head_a);
+void rrb(t_dlst **head_b);
+void rrr(t_dlst **head_a, t_dlst **head_b);
+
+void pa(t_dlst **head_a, t_dlst **head_b);
+void pb(t_dlst **head_a, t_dlst **head_b);
+
+/**************************Все до этого не мое*********************/
+typedef struct	s_push_swap_frame
+{
+    int MIN; //мин. значение
+    int MAX; //макс. значение
+    int LEN; //кол-во символов
+    int MEDIAN; //медиана
+
+    t_dlst *a; //стэк а
+    t_dlst b; //стэк а
+
+    //переменные для  sort_2-5
+	int temp;
+	int top;
+	int mid;
+	int bot;
+}				t_frame;
+
+void 	sort_2(t_dlst **head_a);
+void	sort_3(t_frame *frame);
+void	sort_4(t_frame *frame);
+void	sort_5(t_frame *frame);
+
+#endif //PUSH_SWAP_PUSH_SWAP_H
