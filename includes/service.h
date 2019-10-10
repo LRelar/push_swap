12f253bb57251a7a117a6550bc53b2766ed522b6
ekedge-w <alicemirror1211@gmail.com>
2019-10-10 17:46:10 +0300

    Слияние почти готово p2

--- includes/service.h
+++ includes/service.h
@@ -6,7 +6,7 @@
 /*   By: ekedge-w <ekedge-w@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 18:42:35 by ekedge-w          #+#    #+#             */
-/*   Updated: 2019/10/10 17:21:23 by ekedge-w         ###   ########.fr       */
+/*   Updated: 2019/10/10 17:38:32 by ekedge-w         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,5 +53,6 @@ void	s_split_chunks(t_frame *fr);
 void	free_and_exit(void *arr, void *vol);
 int		find_double(int *arr, t_frame *vol);
 int		digit_cheker(char **av);
+void	parser(int ac, char **av);
 
 #endif //PUSH_SWAP_SERVICE_H
